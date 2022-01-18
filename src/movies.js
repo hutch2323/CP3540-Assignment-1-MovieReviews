9e42970431a7f110fd66f04bdead9d1726978fb5
hutch2323 <marcus.hutchings30@gmail.com>
2022-01-18 01:22:21 -0330

    Updates: 2022-01-18

--- src/movies.js
+++ src/movies.js
@@ -1,63 +1,6 @@
 import { FaTrash } from "react-icons/fa";
-import React, { useState, useRef } from "react";
+import React, { Component, useState, useRef } from "react";
 import { useNavigate } from 'react-router-dom';
-import { axios } from 'axios';
-
-// export function AddReviewForm({ onNewReview = f => f }) {
-//     const [file, setFile] = useState(null);
-//     const movieName = useRef();
-//     const releaseDate = useRef();
-//     const actors = useRef();
-//     const poster = useRef();
-//     const rating = useRef();
-//     const navigate = useNavigate();
-
-//     const submit = e => {
-//         e.preventDefault();
-//         const name = movieName.current.value;
-//         const date = releaseDate.current.value;
-//         const movieActors = actors.current.value.split(" ,");
-//         const moviePoster = poster.current.value;
-//         const movieRating = rating.current.value;
-//         onNewReview(name, date, movieActors, moviePoster, movieRating);
-//         movieName.current.value = "";
-//         releaseDate.current.value = "";
-//         actors.current.value = "";
-//         poster.current.value = "";
-//         rating.current.value = "";
-//         navigate('/');
-//     }
-
-//     const onFileUpload = (e) => {
-//         console.log(e.target.files)
-//         setFile(e.target.value[0])
-//     }
-
-    // return(
-    //     <>
-    //         <form onSubmit={submit}>
-    //             <label for="name">Movie Name:</label>
-    //             <input ref={movieName} type="text" id="name" name="name" required/>
-    //             <label for="date">Release Date:</label>
-    //             <input ref={releaseDate} type="date" id="date" name="date" required/>
-    //             <label for="actors">Actors (separate by commas):</label>
-    //             <input ref={actors} type="text" id="actors" name="actors" required/>
-    //             <label for="image">Poster:</label>
-    //             <input ref={poster} type="file" id="poster" name="poster" accept=".png, .jfif, .jpg, .jpeg" required/>
-    //             <label for="rating">Rating (out of 5):</label>
-    //             <select ref={rating} id="rating" name="rating" required>
-    //                 <option value="0">0</option>
-    //                 <option value="1">1</option>
-    //                 <option value="2">2</option>
-    //                 <option value="3">3</option>
-    //                 <option value="4">4</option>
-    //                 <option value="5">5</option>
-    //             </select>
-    //             <button onClick={onFileUpload()} type="submit" value="Submit">Add Review</button>
-    //         </form>
-    //     </>
-    // );
-// }
 
 export function AddReviewForm({ onNewReview = f => f }){
     const [name, setName] = useState("");
@@ -65,16 +8,22 @@ export function AddReviewForm({ onNewReview = f => f }){
     const [actors, setActors] = useState("");
     const [poster, setPoster] = useState("");
     const [rating, setRating] = useState(0);
+    const navigate = useNavigate();
 
-    const submit = e => {
-        e.preventDefault();
-        // const movieActors = actors.split(", ");
-        onNewReview(name, date, actors, poster, rating);
+    const submit = evt => {
+        evt.preventDefault();
+        onNewReview(name, date, actors.split(", "), poster, rating);
         setName("");
         setDate(null);
         setActors([]);
         setPoster("");
         setRating(0);
+        navigate('/');
+    }
+
+    const onFileChange = evt => {
+        let file = evt.target.files[0];
+        setPoster(URL.createObjectURL(file));
     }
 
     return(
@@ -83,7 +32,7 @@ export function AddReviewForm({ onNewReview = f => f }){
                 <label htmlFor="name">Movie Name:</label>
                 <input 
                     value={name} 
-                    onChange = {event => setName(event.target.value)}
+                    onChange = {evt => setName(evt.target.value)}
                     type="text" 
                     id="name" 
                     name="name" 
@@ -93,7 +42,7 @@ export function AddReviewForm({ onNewReview = f => f }){
                 <label htmlFor="date">Release Date:</label>
                 <input 
                     value={date}
-                    onChange = {event => setDate(event.target.value)}
+                    onChange = {evt => setDate(evt.target.value)}
                     type="date" 
                     id="date" 
                     name="date" 
@@ -103,7 +52,7 @@ export function AddReviewForm({ onNewReview = f => f }){
                 <label htmlFor="actors">Actors (separate by commas):</label>
                 <input 
                     value={actors} 
-                    onChange = {event => setActors((event.target.value).split(", "))}
+                    onChange = {evt => setActors(evt.target.value)}
                     type="text" 
                     id="actors" 
                     name="actors" 
@@ -112,8 +61,8 @@ export function AddReviewForm({ onNewReview = f => f }){
 
                 <label htmlFor="image">Poster:</label>
                 <input 
-                    value={poster}
-                    onChange = {event => setPoster(event.target.value)} 
+                    // value={poster}
+                    onChange = {evt => onFileChange(evt)} 
                     type="file" 
                     id="poster" 
                     name="poster" 
@@ -124,7 +73,7 @@ export function AddReviewForm({ onNewReview = f => f }){
                 <label htmlFor="rating">Rating (out of 5):</label>
                 <select 
                     value={rating}
-                    onChange = {event => setRating(event.target.value)} 
+                    onChange = {evt => setRating(evt.target.value)} 
                     id="rating" 
                     name="rating" 
                     required
