0aa8b522697ad81c53abdde697070ea006b403be
hutch2323 <marcus.hutchings30@gmail.com>
2022-01-19 23:49:31 -0330

    Updates: 2022-01-19

--- src/movies.js
+++ src/movies.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { FaTrash } from "react-icons/fa"; 
+import React, { useState } from "react";
+import { FaTrash, FaStar } from "react-icons/fa"; 
 
 export function MovieList({ movies = [], onRemoveMovie = f => f }){
     if (!movies.length) return <div>No Movie Reviews Available</div>;
@@ -17,16 +17,30 @@ export function MovieList({ movies = [], onRemoveMovie = f => f }){
 
 export function Movie({name, date, actors, poster, rating, onRemove = f => f}) {
     console.log(name, date, actors, poster, rating);
+
+    let stars = [];
+    for(let i = 0; i < rating; i++){
+        stars.push(i);
+    }
+
+    let greyStars = []
+    for(let i = 0; i < ( 5  - rating ); i++){
+        greyStars.push(i)
+    }
+
     return (
       <>
         <div className="card" style={{width: "18rem", padding: "5px", margin: "10px", textAlign: "center"}}>
             <div className="card-header">
                 <h2>{name}</h2>
             </div>
-            <img src={poster} width="100%" height="100%" style={{margin: "auto"}}/>
-            <p>Released: {date}</p>
+            <img className="p-3" src={poster} width="100%" height="100%" style={{margin: "auto"}}/>
+            <p><b>Released:</b> {date}</p>
             {actors.map( actor => {return <p>{actor}</p>})}
-            <p>{rating}</p>
+            <div>
+                {stars.map( star => {return <FaStar color="red"/>})}
+                {greyStars.map( star => { return <FaStar color="grey"/>})}
+            </div>
             <button onClick={() => onRemove(name)}>
                 <FaTrash />
             </button>
--- src/pages.js
+++ src/pages.js
@@ -2,17 +2,29 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { MovieList } from "./movies";
 import { AddReviewForm } from "./addReviewForm";
+import bootstrap from "bootstrap";
+import './App.css';
 
 function NavigationBar(){
     return(
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <Link className="nav-item nav-link active" to="/">Home</Link>
-                    <Link className="nav-item nav-link active" to="/addReview">Add Review</Link>
-                </div>
-            </div>
-        </nav>
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div id="navContainer" class="container-fluid">
+                    <img id="logo" src="images/movieReviews.png" height="100px" width="600px"/>
+                        <div className="navbar-nav" style={{width:"100%", position:"relative",float:"right"}}>
+                            <ul class="navbar-nav w-100 nav-fill mx-auto order-0">
+                                <li id="firstLink" class="nav-item fs-5" >
+                                    <Link className="nav-item nav-link active" to="/">Home</Link>
+                                </li>
+                                <li class="nav-item fs-5">
+                                    <Link className="nav-item nav-link active" to="/addReview">Add Review</Link>
+                                </li>	
+                            </ul>
+                        </div>
+                    </div>
+            </nav>
+
+        </>
     )
 }
 
